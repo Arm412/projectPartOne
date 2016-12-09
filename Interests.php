a8c10cf879a46fd58301c6b2517826cc7b61d1f0
Adam Mitro <Arm148@pitt.edu>
2016-12-08 19:17:17 -0500

    Changed file extensions to php

---
+++ Interests.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="css/WebsiteCSS.css">
+	<title>Adam's Interests</title>
+	<meta charset="UTF-8">
+	<meta content="author: Adam-Mitro">
+</head>
+<body id="body">
+<div class="header" id="paws"></div>
+<div class="header-transparent" id="opac">
+	<div class="head-title-div" id="headDiv">
+		<h1 class="header-title" id="head">Interests</h1>
+	</div>
+	<div class="navigation-bar">
+		<a href="index.php" class="body-information-div">
+			<p class="yellow-text">Home</p>
+		</a>
+		<a href="interests.php" id="mid2" class="body-information-div">
+			<p class="yellow-text">Interests</p>
+		</a>
+		<a href="Misc.php" id="mid1" class="body-information-div">
+			<p class="yellow-text">Miscellaneous</p>
+		</a>
+		<a href="sports.php" class="body-information-div">
+			<p class="yellow-text">Sports</p>
+		</a>
+	</div>
+</div>
+<a href="ContactMe.php">
+	<div class="yellow-text body-information-div" id="contactMeDiv">
+		<p>Contact Me</p>
+	</div>
+</a>
+<div id="sideBodyInterests" class="body-information-div">
+	
+</div>
+<div id="rightBodyInterests" class="body-information-div">
+	
+</div>
+<div id="bodyDiv" class="body-information-div">
+	<div class="yellow-text">
+		<div class="interests">
+			<h1 class="center">Gaming</h1>
+			<h2>Current Favorite Games</h2>
+			<ul>
+				<li>Overwatch</li>
+				<li>Smite</li>
+				<li>Grand Theft Auto 5</li>
+				<li>Battlefield 1</li>
+			</ul>
+			<h2>Older Games That I liked</h2>
+			<ul>
+				<li>Ratchet and Clank</li>
+				<li>Jak and Daxter</li>
+				<li>Spyro</li>
+				<li>Tak</li>
+				<li>Spyro</li>
+				<li>Maplestory</li>
+				<li>Runescape</li>
+				<li>Super Smash Brothers</li>
+			</ul>
+		</div>
+	</div>
+	<div class="yellow-text">
+		<div class="interests">
+			<h1 class="center">Music</h1>
+			<h2>Favorite Type Of Music</h2>
+			<ul>
+				<li>Electronic</li>
+				<li>Pop</li>
+				<li>Rap</li>
+			</ul>
+		</div>
+	</div>
+	<div class="yellow-text">
+		<div class="interests">
+			<h1 class="center">Board Games</h1>
+			<h2>Current Favorite Board Games</h2>
+			<ul>
+				<li>Shadows Over Camelot</li>
+				<li>Settlers of Catan</li>
+				<li>7 Wonders</li>
+				<li>Dead Of Winter</li>
+			</ul>
+		</div>
+	</div>
+	<div class="yellow-text">
+		<div class="interests">
+			<h1 class="center">Movies</h1>
+			<h2>Types of Movies I Watch</h2>
+			<ul>
+				<li>Comedy</li>
+				<li>Animated</li>
+				<li>Action</li>
+			</ul>
+			<h2>Favorite Movies</h2>
+			<ul>
+				<li>Inception</li>
+				<li>Finding Dory</li>
+				<li>SuperBad</li>
+			</ul>
+		</div>
+	</div>
+</div>
+<footer id="foot">
+	<div class="contact">
+		<nav class="contact-bar"></nav>
+		<nav class="contact-bar"></nav>
+		<nav class="contact-bar">
+			<p class="yellow-text">Social Media: </p>
+			<a href="https://www.facebook.com/adam.mitro.9">
+				<img class="black-border" alt="Facebook" src="Images/facebook.png" height="30" width="30">
+			</a>
+			<a href="https://www.instagram.com/svadam95/">
+				<img class="black-border" alt="Instagram" src="Images/instagram.png" height="30" width="30">
+			</a>
+			<a href="https://twitter.com/svadam412">
+				<img class="black-border" alt="Twitter" src="Images/twitter.png" height="30" width="30">
+			</a>
+		</nav>
+		<nav class="contact-bar"></nav>
+		<nav class="contact-bar"></nav>
+	</div>
+</footer>
+<script type="text/javascript" src="http://ajax.aspnetcdn.com/ajax/jquery/jquery-1.4.1.min.js"></script>
+<script src="js/index.js"></script>
+</body>
+</html>
---
+++ Misc.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="css/WebsiteCSS.css">
+	<title>Miscellaneous</title>
+	<meta charset="UTF-8">
+	<meta content="author: Adam-Mitro">
+</head>
+<body id="body">
+<div class="header" id="paws"></div>
+<div class="header-transparent" id="opac">
+	<div class="head-title-div" id="headDiv">
+		<h1 class="header-title" id="head">Miscellaneous</h1>
+	</div>
+	<div class="navigation-bar">
+		<a href="index.php" class="body-information-div">
+			<p class="yellow-text">Home</p>
+		</a>
+		<a href="interests.php" id="mid2" class="body-information-div">
+			<p class="yellow-text">Interests</p>
+		</a>
+		<a href="Misc.php" id="mid1" class="body-information-div">
+			<p class="yellow-text">Miscellaneous</p>
+		</a>
+		<a href="sports.php" class="body-information-div">
+			<p class="yellow-text">Sports</p>
+		</a>
+	</div>
+</div>
+<a href="ContactMe.php">
+	<div class="yellow-text body-information-div" id="contactMeDiv">
+		<p>Contact Me</p>
+	</div>
+</a>
+<div class="body-information-div" id="sideBodyMisc">
+	<div class="yellow-text">
+		<p>If you want to try out the games I made in my free time you can check these ones out.</p>
+		<p>They will not be pretty but it was my first time trying it out.</p><br>
+		<p>Press Space to jump. Its supposed to be like Flappy Bird.</p>
+		<a href="FlappyGame/Flappy.html">
+			<img class="game_size" src="FlappyGame/Img/PlayerRed.png">
+		</a>
+		<br><br><br>
+		<p>Use the arrow keys to move and avoid the ghosts.</p>
+		<a href="MyGame/MyGame.html">
+			<img class="game_size" src="MyGame/Img/Player1.png">
+		</a>
+	</div>
+</div>
+<div class="body-information-div" id="rightBodyMisc">
+	<img id="sidePicture" src="Images/Programming-Joke.png">
+	<br><br><br>
+	<div class="yellow-text" id="boxedIn">
+		<h1>Dec 25</h1>
+		<h1>is equal to</h1>
+		<h1>Oct 31</h1>
+	</div>
+</div>
+<div class="body-information-div" id="bodyDiv">
+	<div id="narrow">
+		<h2 class="yellow-text">Click the target to go to a random web page!</h2>
+	</div>
+	<button id="miscButton"></button>
+	<img src="Images/arrow.png" id="arrow">
+	<div id="pictureHolder" class="yellow-text">
+		<h2>Here are some random stuff I find interesting/funny. Enjoy!</h2>
+		<img src="Images/ImSorryMatt.png" alt="I'm sorry Matt" class="miscImg">
+		<img src="Images/jsPic.png" class="miscImg" width="500" height="350">
+		<img src="Images/Recursion.png" class="miscImg" width="500" height="600">
+	</div>
+</div>
+<footer id="foot">
+	<div class="contact">
+		<nav class="contact-bar"></nav>
+		<nav class="contact-bar"></nav>
+		<nav class="contact-bar">
+			<p class="yellow-text">Social Media: </p>
+			<a href="https://www.facebook.com/adam.mitro.9">
+				<img class="black-border" alt="Facebook" src="Images/facebook.png" height="30" width="30">
+			</a>
+			<a href="https://www.instagram.com/svadam95/">
+				<img class="black-border" alt="Instagram" src="Images/instagram.png" height="30" width="30">
+			</a>
+			<a href="https://twitter.com/svadam412">
+				<img class="black-border" alt="Twitter" src="Images/twitter.png" height="30" width="30">
+			</a>
+		</nav>
+		<nav class="contact-bar"></nav>
+		<nav class="contact-bar"></nav>
+	</div>
+</footer>
+<script type="text/javascript" src="http://ajax.aspnetcdn.com/ajax/jquery/jquery-1.4.1.min.js"></script>
+<script src="js/index.js"></script>
+</body>
+</html>
